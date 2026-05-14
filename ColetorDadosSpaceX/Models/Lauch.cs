ab2bebdbf582a18820835bc9c0f3f25c10297ed9
Miguel Duarte <miguelcassiob@gmail.com>
2026-05-14 19:48:19 -0300

    Adicionar arquivos de projeto.

---
+++ ColetorDadosSpaceX/Models/Lauch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ColetorDadosSpaceX.Models
+{
+    public class Launch
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("date_utc")]
+        public DateTime DateUtc { get; set; }
+
+        // O sucesso pode ser nulo na API da SpaceX se o voo ainda não aconteceu ou foi cancelado antes
+        [JsonPropertyName("success")]
+        public bool? Success { get; set; }
+
+        [JsonPropertyName("details")]
+        public string Details { get; set; }
+    }
+}
---
+++ ColetorDadosSpaceX/Models/Rocket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ColetorDadosSpaceX.Models
+{
+    public class Rocket
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("active")]
+        public bool Active { get; set; }
+
+        [JsonPropertyName("success_rate_pct")]
+        public double SuccessRatePct { get; set; }
+    }
+}
